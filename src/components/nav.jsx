3adf5a3d430dd6199f98d7681f488395bbcecd48
Li Chaoran <pkwarcraft@gmail.com>
2016-06-23 20:01:02 +0800

    edit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,5 +2,10 @@ import React  from 'react';
 import { Link } from 'react-router';
 
 export var Nav = React.createClass({
-
+    render: function () {
+        return (
+            <div>
+            </div>
+        );
+    }
 });
