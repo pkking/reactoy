264efb4127422dec417af328d80621f9210d814d
Li Chaoran <pkwarcraft@gmail.com>
2016-07-01 16:47:16 +0800

    edit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,7 +6,6 @@ export var About = React.createClass({
 		return (
 			<div className="content pure-u-1-1 pure-u-md-3-4">
 				<p> this is about </p>
-				<Footer />
 			</div>
 		);
 	}
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,7 +4,19 @@ import { Link } from 'react-router';
 export var Nav = React.createClass({
     render: function () {
         return (
-            <div>
+            <div className="nav pure-menu pure-menu-horizontal">
+                <ul className="pure-menu-list">
+                    <li className="pure-menu-item"><Link to="/" className="pure-menu-link">首页</Link></li>
+                    <li className="pure-menu-item"><Link to="/about" href="#" className="pure-menu-link">关于</Link></li>
+                    <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                        <Link to="/" id="menuLink1" className="pure-menu-link">社交</Link>
+                        <ul className="pure-menu-children">
+                            <li className="pure-menu-item"><Link href="#" className="pure-menu-link">Email</Link></li>
+                            <li className="pure-menu-item"><Link href="#" className="pure-menu-link">Twitter</Link></li>
+                            <li className="pure-menu-item"><Link href="#" className="pure-menu-link">Tumblr Blog</Link></li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         );
     }
